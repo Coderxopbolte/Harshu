68f6d26e411cf784ee1266a4d6aaddb2e6d778c6
Royalkifeelings <116754209+royalboycoder@users.noreply.github.com>
2023-03-04 00:25:04 +0530

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -9,17 +9,17 @@ def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="🌹 𝗔𝗱𝗱 𝗠𝗲 𝗧𝗼 𝗬𝗼𝘂𝗿 𝗚𝗿𝗼𝘂𝗽 🌹",
+                text="✯𝗔𝗱𝗱 𝗠𝗲 𝗧𝗼 𝗬𝗼𝘂𝗿 𝗚𝗿𝗼𝘂𝗽✯",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
         [
             InlineKeyboardButton(
-                text="🌹 𝗛𝗲𝗹𝗽 🌹",
+                text="✯𝗛𝗲𝗹𝗽✯",
                 callback_data="settings_back_helper",
             ),
             InlineKeyboardButton(
-                text="🌹 𝗦𝗲𝘁𝘁𝗶𝗻𝗴𝘀 🌹", callback_data="settings_helper"
+                text="✯ 𝗦𝗲𝘁𝘁𝗶𝗻𝗴𝘀 ✯", callback_data="settings_helper"
             ),
         ],
      ]
@@ -30,26 +30,26 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="🌹 𝗔𝗱𝗱 𝗠𝗲 𝗧𝗼 𝗬𝗼𝘂𝗿 𝗚𝗿𝗼𝘂𝗽 🌹",
+                text="✯ 𝗔𝗱𝗱 𝗠𝗲 𝗧𝗼 𝗬𝗼𝘂𝗿 𝗚𝗿𝗼𝘂𝗽 ✯",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
         [
             InlineKeyboardButton(
-                text="🌹 𝗛𝗲𝗹𝗽 🌹", callback_data="settings_back_helper"
+                text="✯ 𝗛𝗲𝗹𝗽 ✯", callback_data="settings_back_helper"
             )
         ],
         [
             InlineKeyboardButton(
-                text="🌹 𝗦𝘂𝗽𝗽𝗼𝗿𝘁 🌹", url=config.SUPPORT_GROUP
+                text="✯ 𝗦𝘂𝗽𝗽𝗼𝗿𝘁 ✯", url=config.SUPPORT_GROUP
             ),
             InlineKeyboardButton(
-                text="🌹 𝗨𝗽𝗱𝗮𝘁𝗲𝘀 🌹", url=f"https://t.me/WCFnetwork",
+                text="✯ 𝗨𝗽𝗱𝗮𝘁𝗲𝘀 ✯", url=f"https://t.me/vick_network",
             )
         ],
         [
             InlineKeyboardButton(
-                text="🌹 𝗢𝘄𝗻𝗲𝗿 🌹", url=f"https://t.me/Kalakar_Sangram",
+                text="👑 𝗢𝘄𝗻𝗲𝗿 👑", url=f"NISHU_OP_OFFICIAL",
             )
         ],
      ]
