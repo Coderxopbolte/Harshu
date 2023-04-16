04e96efd01cd6862a7d73b5786cdf4246b93f6b9
Harshu coder <118929686+Coderxopbolte@users.noreply.github.com>
2023-04-16 23:11:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID"))
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "Vick Music Bot")
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "lemon music bot")
 
 OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
 
@@ -25,14 +25,14 @@ UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/OpSahubot/Vc-Player"
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL","vick_network")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "THE_NETWORK_HUB_SUPPORT")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL","rfs_support ")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "RFS_EMPIRE")
 
 
-DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", "1000"))
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", "10000"))
 SONG_DOWNLOAD_DURATION = int(getenv("SONG_DOWNLOAD_DURATION_LIMIT", "180"))
 
-AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "False")
+AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "True")
 AUTO_LEAVE_ASSISTANT_TIME = int(
     getenv("ASSISTANT_LEAVE_TIME", "5400")
 )
@@ -75,7 +75,7 @@ clean = {}
 autoclean = []
 
 
-START_IMG_URL = getenv("START_IMG_URL", "https://telegra.ph/file/0143fcd82e92b12af954c.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/34c0d7cb37d358434cb27.jpg  ")
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
